04806d8b3eb4eba72e1e21714b776948c2edae9d
SupForeverCloud <124637592+SupForeverCloud@users.noreply.github.com>
2023-06-10 20:36:40 +0200

    Update Java-Sk.js

--- Java-Sk.js
+++ Java-Sk.js
@@ -8,7 +8,7 @@ function openCustomPageDialog(primaryControl, firstSelectedItemId, selectedEntit
     target: 2,
     position: 1,
     height: {
-      value: 300,
+      value: 305,
       unit: "px"
     },
     width: {
