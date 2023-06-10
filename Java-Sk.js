9bfed98049428d0b87c24439f729c97b0819f2ce
SupForeverCloud <124637592+SupForeverCloud@users.noreply.github.com>
2023-06-10 19:08:49 +0200

    Update Java-Sk.js

--- Java-Sk.js
+++ Java-Sk.js
@@ -1,33 +1,31 @@
-function openCustomPageDialog(primaryControl, firstSelectedItemId, selectedEntityTypeName)
-{
-    // Centered Dialog
-    var pageInput = {
-        pageType: "custom",
-        name: "cr6f5_popupaosuche_f87e7",
- 
-    };
-    var navigationOptions = {
-        target: 2,
-        position: 1,
-	height: {
-	    value: 300,
-	    unit: "px"
-	},
-	width: {
-	    value: 700,
-	    unit: "px"
-	},
-	title: "Edit Comments",
-    	displayFullScreenButton: false // Set fullscreen button to false
-    };
-    Xrm.Navigation.navigateTo(pageInput, navigationOptions).then(
-        function () {
-            // Refresh the main form when the dialog is closed
-            primaryControl.data.refresh();
-        }
-    ).catch (
-        function (error) {
-            // Handle error
-        }
-    );
+function openCustomPageDialog(primaryControl, firstSelectedItemId, selectedEntityTypeName) {
+  // Centered Dialog
+  var pageInput = {
+    pageType: "custom",
+    name: "cr6f5_popupaosuche_f87e7",
+  };
+  var navigationOptions = {
+    target: 2,
+    position: 1,
+    height: {
+      value: 300,
+      unit: "px"
+    },
+    width: {
+      value: 700,
+      unit: "px"
+    },
+    title: "Edit Comments",
+    displayFullScreenButton: false // Set fullscreen button to false
+  };
+  Xrm.Navigation.navigateTo(pageInput, navigationOptions).then(
+    function () {
+      // Refresh the main form when the dialog is closed
+      primaryControl.data.refresh();
+    }
+  ).catch(
+    function (error) {
+      // Handle error
+    }
+  );
 }
