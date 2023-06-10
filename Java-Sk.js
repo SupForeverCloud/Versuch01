d364f47021374050a0cd4e27daf397596a3c7dfe
SupForeverCloud <124637592+SupForeverCloud@users.noreply.github.com>
2023-06-10 16:14:13 +0200

    Update Java-Sk.js

--- Java-Sk.js
+++ Java-Sk.js
@@ -1,33 +1,21 @@
-function openCustomPageDialog(primaryControl, firstSelectedItemId, selectedEntityTypeName)
-{
-    // Centered Dialog
-    var pageInput = {
-        pageType: "custom",
-        name: "cr5b0_reviewmodal_57388",
-        entityName: selectedEntityTypeName, // "sample_review"
-        recordId: firstSelectedItemId // "{087AA308-B321-E811-A845-000D3A33A3AC}" 
-    };
-    var navigationOptions = {
-        target: 2,
-        position: 1,
-	height: {
-	    value: 260,
-	    unit: "px"
-	},
-	width: {
-	    value: 50,
-	    unit: "%"
-	},
-	title: "Edit Comments"
-    };
-    Xrm.Navigation.navigateTo(pageInput, navigationOptions).then(
+// Centered Dialog
+var pageInput = {
+    pageType: "custom",
+    name: "cr6f5_popupaosuche_f87e7",
+};
+var navigationOptions = {
+    target: 2, 
+    position: 1,
+    width: {value: 50, unit:"%"},
+    title: "<dialog title>"
+};
+Xrm.Navigation.navigateTo(pageInput, navigationOptions)
+    .then(
         function () {
-            // Refresh the main form when the dialog is closed
-            primaryControl.data.refresh();
+            // Called when the dialog closes
         }
-    ).catch (
+    ).catch(
         function (error) {
             // Handle error
         }
     );
-}
