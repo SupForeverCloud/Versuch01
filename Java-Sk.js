fb820275853a0d587c4d45a65ec395f9f48d0480
SupForeverCloud <124637592+SupForeverCloud@users.noreply.github.com>
2023-06-16 12:37:53 +0200

    Update Java-Sk.js

--- Java-Sk.js
+++ Java-Sk.js
@@ -17,7 +17,7 @@ function openCustomPageDialog(primaryControl, firstSelectedItemId, selectedEntit
       value: 700,
       unit: "px"
     },
-    title: "Edit Comments",
+    title: "Anschlussobjekt suchen",
     displayFullScreenButton: false // Set fullscreen button to false
   };
   Xrm.Navigation.navigateTo(pageInput, navigationOptions).then(
