0f9f0d567719145b0fb4ae844c8f8b6a7b4fe834
SupForeverCloud <124637592+SupForeverCloud@users.noreply.github.com>
2023-06-22 00:55:01 +0200

    Update Java-Sk.js

--- Java-Sk.js
+++ Java-Sk.js
@@ -1,9 +1,9 @@
-function openCustomPageDialog(primaryControl, firstSelectedItemId, selectedEntityTypeName) {
+function openCustomPageDialog(primaryControl, firstSelectedItemId, VarParameter) {
   // Centered Dialog
   var pageInput = {
     pageType: "custom",
     name: "cr6f5_popupaosuche_f87e7",
-    entityName: "hallo",
+    entityName: VarParameter,
     recordId: firstSelectedItemId // "{087AA308-B321-E811-A845-000D3A33A3AC}" 
   };
   var navigationOptions = {
