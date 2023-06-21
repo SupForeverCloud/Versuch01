e8a22323b26c9a03d682bcdad4e470e5a78c575e
SupForeverCloud <124637592+SupForeverCloud@users.noreply.github.com>
2023-06-21 23:35:04 +0200

    Update Java-Sk.js

--- Java-Sk.js
+++ Java-Sk.js
@@ -2,7 +2,7 @@ function openCustomPageDialog(primaryControl, firstSelectedItemId, selectedEntit
   // Centered Dialog
   var pageInput = {
     pageType: "custom",
-    name: "cr6f5_popupaosuche_f87e7&parametername=parametervalue",
+    name: "cr6f5_popupaosuche_f87e7",
     entityName: selectedEntityTypeName, // "sample_review"
     recordId: firstSelectedItemId // "{087AA308-B321-E811-A845-000D3A33A3AC}" 
   };
